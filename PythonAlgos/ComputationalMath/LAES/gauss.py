bfe4be0b16a4ec435f289c9c321c483aec20632a
Marise Hayashi <limitedeternity@users.noreply.github.com>
2019-11-05 17:11:27 +0300

    Update gauss.py

--- PythonAlgos/ComputationalMath/LAES/gauss.py
+++ PythonAlgos/ComputationalMath/LAES/gauss.py
@@ -11,10 +11,10 @@ class GaussMatrix:
     
   def trianglize(self):
     for col in range(len(self.m[0]) - 2):
-      print(f"Selected column {col}")
+      print(f"Selected column {col + 1}")
 
       for i in range(col, len(self.m) - 1):
-        print(f"Substituting row {i + 1} with row 0 mulitplied by {-(self.m[i + 1][col] / self.m[col][col])}")
+        print(f"Substituting row {i + 2} with row {col + 1} mulitplied by {-(self.m[i + 1][col] / self.m[col][col])}")
         self.substitute_row(i + 1, col, -(self.m[i + 1][col] / self.m[col][col]))
       
         for t in self.m:
@@ -30,12 +30,11 @@ class GaussMatrix:
         sols[i] = self.m[i][len(self.m[i]) - 1] / self.m[i][i]
       
       else:
-        accum = list(map(lambda q, x: q*x, self.m[i][len(self.m[i]) - 1 - len(list(filter(None, sols))):len(self.m[i]) - 1], filter(None, sols)))
+        accum = list(map(lambda q,x: q*x, self.m[i][len(self.m[i]) - 1 - len(list(filter(None, sols))):len(self.m[i]) - 1], filter(None, sols)))
         sols[i] = (self.m[i][len(self.m[i]) - 1] - sum(accum)) / self.m[i][i]
     
     for i, t in enumerate(sols, 1):
       print(f"x{i}={t}")
-    
 
   def substitute_row(self, target: int, source: int, mul_by: int):
     self.m[target] = list(map(lambda a,b: a+b, self.m[target], map(lambda x: x*mul_by, self.m[source])))
@@ -44,4 +43,3 @@ class GaussMatrix:
 gm = GaussMatrix()
 gm.trianglize()
 gm.solve()
-
