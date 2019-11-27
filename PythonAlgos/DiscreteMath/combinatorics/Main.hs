b04394536f36078558ad47a14b75a4e734d033b7
Marise Hayashi <limitedeternity@users.noreply.github.com>
2019-11-27 18:54:20 +0300

    Add files via upload

--- PythonAlgos/DiscreteMath/combinatorics/Main.hs
+++ PythonAlgos/DiscreteMath/combinatorics/Main.hs
@@ -5,7 +5,11 @@ a :: Int -> Int -> Double
 a k n = (fromInteger $ factorial n) / (fromInteger $ factorial (n - k))
 
 a_ k n = power n k
+
 p n = factorial n
+
+p_ :: Int -> [Int] -> Double
+p_ n lst = (fromInteger $ factorial n) / (foldl (*) 1 (map (fromInteger . factorial) lst)) 
 
 c :: Int -> Int -> Double
 c k n = (fromInteger $ factorial n) / ((fromInteger $ factorial k) * (fromInteger $ factorial (n - k)))
