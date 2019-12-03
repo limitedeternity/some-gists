d852037c3e3c7df172ea9e03c12ebf168aa5aa2f
Marise Hayashi <limitedeternity@users.noreply.github.com>
2019-12-03 14:37:20 +0300

    Add files via upload

---
+++ PythonAlgos/ComputationalMath/Interpolation/lagrange.py
@@ -0,0 +1,25 @@
+import math
+from itertools import combinations
+
+from sympy import sympify, simplify
+
+
+x = [1.375, 1.38, 1.385, 1.39, 1.395, 1.4]
+y = [5.04192, 5.17744, 5.32016, 5.47069, 5.62968, 5.79788]
+assert len(x) == len(y)
+assert len(x) > 1
+
+poly = []
+numerator_combinations = list(combinations(range(len(x)), len(x) - 1))[::-1]
+
+for i, n in enumerate(numerator_combinations):
+    denominator = eval(' * '.join(map(lambda t: f'({x[i] - x[t]})', n)))
+    poly.append(
+        f"{y[i] / denominator} * {' * '.join(map(lambda t: f'(x - {x[t]})', n))}" 
+    )
+
+print("\n\n+\n\n".join(poly), '\n\n')
+
+expr = sympify(" + ".join(poly), evaluate=False)
+print(simplify(expr))
+
