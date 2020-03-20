8fb081ec53783587efd1b91e970fa98772b55eb0
Marise Hayashi <limitedeternity@users.noreply.github.com>
2020-03-20 16:51:44 +0300

    Update build_logical_table.py

--- PythonAlgos/RelationalAlgebra/build_logical_table.py
+++ PythonAlgos/RelationalAlgebra/build_logical_table.py
@@ -1,12 +1,8 @@
 from itertools import product
 
-
-two_arg = list(product(range(2), repeat=2))
-three_arg = list(product(range(2), repeat=3))
-
-def build_table(combinations, fn):
+def build_table(args_amount, fn):
+    combinations = list(product(range(2), repeat=args_amount))
     return list(zip(combinations, map(lambda comb: bool(fn(*comb)), combinations)))
 
-print("\n".join(str(t) for t in build_table(two_arg, lambda x, y: ((not(x) or y) and x) == y)))
+print("\n".join(str(t) for t in build_table(2, lambda x, y: ((not(x) or y) and x) == y)))
 print()
-
