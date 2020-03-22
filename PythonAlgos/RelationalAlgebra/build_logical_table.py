0443ac27a20bff2c22ab7b68180296d31e15dc2c
Marise Hayashi <limitedeternity@users.noreply.github.com>
2020-03-22 10:33:21 +0300

    Delete build_logical_table.py

--- PythonAlgos/RelationalAlgebra/build_logical_table.py
+++
@@ -1,8 +0,0 @@
-from itertools import product
-
-def build_table(args_amount, fn):
-    combinations = list(product(range(2), repeat=args_amount))
-    return list(zip(combinations, map(lambda comb: bool(fn(*comb)), combinations)))
-
-print("\n".join(str(t) for t in build_table(2, lambda x, y: ((not(x) or y) and x) == y)))
-print()
