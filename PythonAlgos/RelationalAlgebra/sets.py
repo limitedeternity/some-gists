3a6b777e279c0be2fcec97965214a3cb46fdf2e3
Marise Hayashi <limitedeternity@users.noreply.github.com>
2020-03-28 13:16:50 +0300

    Add files via upload

---
+++ PythonAlgos/RelationalAlgebra/sets.py
@@ -0,0 +1,29 @@
+from copy import copy
+from re import sub
+
+input = "{(11110),(11011),(10101),(01110),(10001),(01101),(11101),(00100)}"
+input = sub(r"(\d)", r"\1,", input.strip())
+input = input.replace("{", "[").replace("}", "]")
+a = eval(input)
+
+
+print("Соседние наборы:")
+found_neighbours = []
+for s in a:
+    for idx in range(len(s)):
+        s_copy = list(s)
+        s_copy[idx] = int(not s_copy[idx])
+        s_copy = tuple(s_copy)
+        if s_copy in a and {s, s_copy} not in found_neighbours:
+            found_neighbours.append({s, s_copy})
+            print({s, s_copy})
+
+
+print("\nПротивоположные наборы:")
+found_opposites = []
+for s in a:
+    s_copy = tuple(map(lambda i: int(not i), s))
+    if s_copy in a and {s, s_copy} not in found_opposites:
+        found_opposites.append({s, s_copy})
+        print({s, s_copy})
+
