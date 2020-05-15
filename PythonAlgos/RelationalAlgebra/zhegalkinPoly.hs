efceb8a924b8513d79fcb9876eda80ca10144644
Marise Hayashi <vyatheslav2707@mail.ru>
2020-05-15 21:09:22 +0300

    Update zhegalkinPoly.hs

--- PythonAlgos/RelationalAlgebra/zhegalkinPoly.hs
+++ PythonAlgos/RelationalAlgebra/zhegalkinPoly.hs
@@ -6,6 +6,16 @@ import Data.List (intercalate)
 import LogicTable hiding (main)
 
 
+data Cond a = a :? a
+
+infixl 0 ?
+infixl 1 :?
+
+(?) :: Bool -> Cond a -> a
+True  ? (x :? _) = x
+False ? (_ :? y) = y
+
+
 zhegalkinReduce :: [([Bool], Bool)] -> [([Bool], Bool)]
 zhegalkinReduce table = [snd x | x <- (zip [0..] table), (fst x) `elem` (map (fst) $ filter (snd) $ zip [0..] $ map (!!0) $ buildXorPyramid $ map (snd) $ table)]
     where
@@ -26,24 +36,26 @@ main = do
 
     let zhegalkinTable = zhegalkinReduce table
     putStrLn "Полином Жегалкина:"
-    putStrLn $ intercalate " |^| " $ map (intercalate " |*| ") $ map (map (\p -> (!!) (varNames) (fst p))) $ map (filter (snd)) $ map (zip [0..]) $ map (fst) $ zhegalkinTable
+    putStrLn $ intercalate " |^| " $ map (intercalate " |*| ") $ (\conj -> (>) (length conj) 0 ? conj :? [["False"]]) $ (flip map) (map (filter (snd)) $ map (zip [0..]) $ map (fst) $ zhegalkinTable) $ \trueVarsPairs -> 
+        if | (>) (length trueVarsPairs) 0 -> map (\p -> (!!) (varNames) (fst p)) trueVarsPairs
+           | otherwise -> ["True"]
     putStrLn ""
 
     putStrLn "СДНФ:"
     putStrLn $ (\s -> "(" ++ s ++ ")") $ intercalate ") |+| (" $ map (intercalate " |*| ") $ (flip map) (map (zip [0..]) $ map (fst) $ filter (snd) $ table) $ map $ \p -> 
-        if | (snd p == True) -> (!!) (varNames) (fst p) 
+        if | (snd p == True) -> (!!) (varNames) (fst p)
            | otherwise -> (++) "!" $ (!!) (varNames) (fst p)
     putStrLn ""
     
     putStrLn "СКНФ:"
     putStrLn $ (\s -> "(" ++ s ++ ")") $ intercalate ") |*| (" $ map (intercalate " |+| ") $ (flip map) (map (zip [0..]) $ map (fst) $ filter (not . snd) $ table) $ map $ \p -> 
-        if | (snd p == False) -> (!!) (varNames) (fst p) 
+        if | (snd p == False) -> (!!) (varNames) (fst p)
            | otherwise -> (++) "!" $ (!!) (varNames) (fst p)
     putStrLn ""
 
     where
         -- Функцию заносить сюда:
-        fn x y z = (y |+| not (x |*| z)) |*| not (y |*| z ↓ x)
+        fn = buildFnFromVec8 [True, False, True, True, True, True, False, False]
         -- Дополнительные переменные:
         varNames = ["x", "y", "z"]
 
